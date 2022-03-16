3e28851cc9006015fc587c390c9e0420d1ccdb2c
immerensis <immerensis@protonmail.com>
2022-03-16 09:47:14 +0530

    fix

--- components/voxel-bear.js
+++ components/voxel-bear.js
@@ -2,7 +2,6 @@ import { useState, useEffect, useRef, useCallback } from 'react'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { loadGLTFModel } from '../lib/model'
-import { bearSpinner, bearContainer } from './voxel-bear-loader'
 
 function easeOutCirc(x) {
   return Math.sqrt(1 - Math.pow(x - 1, 4))
